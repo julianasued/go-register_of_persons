1b77ad02f8408f5999e8b3cd5c8304e73adb2174
julianasued <julianasuedcc@gmail.com>
2022-06-06 20:49:05 -0300

    ultimos envios

---
+++ banco.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `Employee`;
+CREATE TABLE `Employee` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `city` varchar(30) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
