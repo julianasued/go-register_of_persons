f2ae946c9463f46e981df805c4f82688da84577d
julianasued <julianasuedcc@gmail.com>
2022-06-05 21:28:57 -0300

    implementando novos campos no formulario

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ type Employee struct {
     Id    int
     Name  string
     City string
+    Username string
+    Email string
 }
 
 func dbConn() (db *sql.DB) {
@@ -41,14 +43,16 @@ func Index(w http.ResponseWriter, r *http.Request) {
     res := []Employee{}
     for selDB.Next() {
         var id int
-        var name, city string
-        err = selDB.Scan(&id, &name, &city)
+        var name, city, username, email string
+        err = selDB.Scan(&id, &name, &city, &username, &email)
         if err != nil {
             panic(err.Error())
         }
         emp.Id = id
         emp.Name = name
         emp.City = city
+        emp.Username = username
+        emp.Email = email
         res = append(res, emp)
     }
     html.ExecuteTemplate(w, "Index", res)
@@ -65,14 +69,16 @@ func Show(w http.ResponseWriter, r *http.Request) {
     emp := Employee{}
     for selDB.Next() {
         var id int
-        var name, city string
-        err = selDB.Scan(&id, &name, &city)
+        var name, city, username, email string
+        err = selDB.Scan(&id, &name, &city, &username, &email)
         if err != nil {
             panic(err.Error())
         }
         emp.Id = id
         emp.Name = name
         emp.City = city
+        emp.Username = username
+        emp.Email = email
     }
     html.ExecuteTemplate(w, "Show", emp)
     defer db.Close()
@@ -92,14 +98,16 @@ func Edit(w http.ResponseWriter, r *http.Request) {
     emp := Employee{}
     for selDB.Next() {
         var id int
-        var name, city string
-        err = selDB.Scan(&id, &name, &city)
+        var name, city, username, email string
+        err = selDB.Scan(&id, &name, &city, &username, &email)
         if err != nil {
             panic(err.Error())
         }
         emp.Id = id
         emp.Name = name
         emp.City = city
+        emp.Username = username
+        emp.Email = email
     }
     html.ExecuteTemplate(w, "Edit", emp)
     defer db.Close()
@@ -110,12 +118,14 @@ func Insert(w http.ResponseWriter, r *http.Request) {
     if r.Method == "POST" {
         name := r.FormValue("name")
         city := r.FormValue("city")
-        insForm, err := db.Prepare("INSERT INTO Employee(name, city) VALUES(?,?)")
+        username := r.FormValue("username")
+        email := r.FormValue("email")
+        insForm, err := db.Prepare("INSERT INTO Employee(name, city, username, email) VALUES(?,?,?,?)")
         if err != nil {
             panic(err.Error())
         }
-        insForm.Exec(name, city)
-        log.Println("INSERT: Name: " + name + " | City: " + city)
+        insForm.Exec(name, city, username, email)
+        log.Println("INSERT: Name: " + name + " | City: " + city  + " | Username: " + username  + " | Email: " + email)
     }
     defer db.Close()
     http.Redirect(w, r, "/", 301)
@@ -126,13 +136,15 @@ func Update(w http.ResponseWriter, r *http.Request) {
     if r.Method == "POST" {
         name := r.FormValue("name")
         city := r.FormValue("city")
+        username := r.FormValue("username")
+        email := r.FormValue("email")
         id := r.FormValue("uid")
-        insForm, err := db.Prepare("UPDATE Employee SET name=?, city=? WHERE id=?")
+        insForm, err := db.Prepare("UPDATE Employee SET name=?, city=?, username=?, email=? WHERE id=?")
         if err != nil {
             panic(err.Error())
         }
-        insForm.Exec(name, city, id)
-        log.Println("UPDATE: Name: " + name + " | City: " + city)
+        insForm.Exec(name, city, username, email, id)
+        log.Println("UPDATE: Name: " + name + " | City: " + city  + " | Username: " + username  + " | Email: " + email)
     }
     defer db.Close()
     http.Redirect(w, r, "/", 301)
